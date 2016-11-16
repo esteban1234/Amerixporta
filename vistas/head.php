fa1fd6e2641adc620527f8c50073047bcbb79af8
esteban1234 <tva_libra@hotmail.com>
2016-11-16 09:13:43 -0600

    amerixporta new page

---
+++ vistas/head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+            <title>
+                AMERIXPORTA | SERVICIOS DE CONSTRUCCIÓN
+            </title>
+            <!-- <meta content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" name="viewport"> -->
+            <link href="../css/estilos.css" rel="stylesheet">
+                <!-- <link href="../css/responsivo.css" rel="stylesheet"> -->
+                <link href="https://fonts.googleapis.com/css?family=Oswald|Roboto|Roboto+Condensed" rel="stylesheet">
+                    <link href="../fonts/style.css" rel="stylesheet">
+                    </link>
+                </link>
+            </link>
+        </meta>
+    </head>
+</html>
+<body>
+</body>
---
+++ vistas/index.php
@@ -0,0 +1,58 @@
+
+<?php include 'head.php';?>
+
+
+	<header>
+			<div class="logo">
+
+				<figure class="logofigure">
+					<img src="../img/AMERIXPORTA.png"   width="350" height="200" alt="">
+				</figure>
+
+			</div>
+		<div class="contenedor">
+			<nav class="menu">
+				<ul>
+					<li><a href="Index.php"><span class="icon-home"></span>&nbsp;&nbsp;INICIO</a></li>
+					<li><a href="#"><span class="icon-users"></span>&nbsp;&nbsp;QUIENES SOMOS</a></li>
+					<li><a href="#"><span class="icon-suitcase"></span>&nbsp;&nbsp;SERVICIOS</a></li>
+					<li><a href="#"><span class="icon-old-phone"></span>&nbsp;&nbsp;CONTACTO</a></li>
+				</ul>
+			</nav>
+		</div>
+
+	</header>
+
+
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente necessitatibus, corporis exercitationem a eaque iure animi dolor expedita tempore iste. Dolor, amet officia repellendus unde voluptatum architecto quisquam quod eligendi.</p> <br><br>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente necessitatibus, corporis exercitationem a eaque iure animi dolor expedita tempore iste. Dolor, amet officia repellendus unde voluptatum architecto quisquam quod eligendi.</p> <br><br>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente necessitatibus, corporis exercitationem a eaque iure animi dolor expedita tempore iste. Dolor, amet officia repellendus unde voluptatum architecto quisquam quod eligendi.</p> <br><br>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente necessitatibus, corporis exercitationem a eaque iure animi dolor expedita tempore iste. Dolor, amet officia repellendus unde voluptatum architecto quisquam quod eligendi.</p> <br><br>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente necessitatibus, corporis exercitationem a eaque iure animi dolor expedita tempore iste. Dolor, amet officia repellendus unde voluptatum architecto quisquam quod eligendi.</p> <br><br>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente necessitatibus, corporis exercitationem a eaque iure animi dolor expedita tempore iste. Dolor, amet officia repellendus unde voluptatum architecto quisquam quod eligendi.</p> <br><br>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente necessitatibus, corporis exercitationem a eaque iure animi dolor expedita tempore iste. Dolor, amet officia repellendus unde voluptatum architecto quisquam quod eligendi.</p> <br><br>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente necessitatibus, corporis exercitationem a eaque iure animi dolor expedita tempore iste. Dolor, amet officia repellendus unde voluptatum architecto quisquam quod eligendi.</p> <br><br>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente necessitatibus, corporis exercitationem a eaque iure animi dolor expedita tempore iste. Dolor, amet officia repellendus unde voluptatum architecto quisquam quod eligendi.</p> <br><br>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente necessitatibus, corporis exercitationem a eaque iure animi dolor expedita tempore iste. Dolor, amet officia repellendus unde voluptatum architecto quisquam quod eligendi.</p> <br><br>
+
+
+		<span class="ir-arriba icon-arrow-bold-up"></span>
+
+	<?php include 'Pie.php';?>
+
+    <script type="text/javascript" src="../js/jquery.js"></script>
+	<script src="../js/main.js"></script>
+	<script type="text/javascript" src="../js/iconoarriba.js"></script>
+
+</body>
+</html>
