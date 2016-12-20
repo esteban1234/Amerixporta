587b9739033859f166790c5028d0d891b901af54
champiiy <albert1047@hotmail.com>
2016-12-20 17:50:05 -0600

    Agregando lo elmininado responsivo

---
+++ vistas/headindex.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <title>AMERIXPORTA | SERVICIOS DE CONSTRUCCIÓN</title>
+    <META NAME="Title" content="Somos una compañía ubicada en la ciudad de Mérida, Yucatán, México, dedicados a ofrecer productos para la construcción, construcción de casas, remodelación, contrucción de carreteras, edificios y ampliaciones.">
+    <META NAME="Author" CONTENT="[]"/>
+    <META name="Description" content="Somos una compañía ubicada en la ciudad de Mérida, Yucatán, México, dedicados a ofrecer productos para la construcción, construcción de casas, remodelación, contrucción de carreteras, edificios y ampliaciones."/>
+    <META NAME="Keywords" CONTENT="productos para la construcción merida yucatan, construcción merida yucatan, construcción, AMERIXPORTA merida yucatan, AMERIXPORTA merida yucatan, contrucción de casas merida yucatan, ampliaciones de casa merida yucatan, ingenieria civil merida yucatan, productos de construcción, levantamientos, mexico merida yucatan, desarrollo de proyectos de construcción"/>
+    <!-- <meta content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" name="viewport"> -->
+    <link href="css/estilos.css" rel="stylesheet">
+    <link href="css/responsivo.css" rel="stylesheet">
+        <!-- <link href="../css/responsivo.css" rel="stylesheet"> -->
+    <link href="https://fonts.googleapis.com/css?family=Oswald|Roboto|Roboto+Condensed" rel="stylesheet">
+    <link href="fonts/style.css" rel="stylesheet">
+</head>
+<body>
+
+ <header>
+   <div class="logo">
+     <figure class="logofigure">
+       <img src="img/AMERIXPORTA.png"   width="350" height="200" alt="">
+     </figure>
+     <div class="menuicono">
+       <div id="cerrar">
+         <span>X</span>
+         <p>Cerrar</p>
+       </div>
+       <div id="alternar">
+         <p>Menu</p><span class="icon-menu icono-menu"></span>
+       </div>
+     </div>
+   </div>
+
+
+    <div class="contenedor">
+
+      <nav class="menu" >
+        <ul class="nav">
+          <li><a href="index.php"><span class="icon-home"></span>&nbsp;&nbsp;INICIO</a></li>
+          <li><a href="vistas/quienessomos.php"><span class="icon-users"></span>&nbsp;&nbsp;QUIENES SOMOS</a></li>
+          <li><a href="vistas/productos.php"><span class="icon-suitcase"></span>&nbsp;&nbsp;PRODUCTOS</a></li>
+          <li><a href="vistas/contacto.php"><span class="icon-old-phone"></span>&nbsp;&nbsp;CONTACTO</a></li>
+        </ul>
+      </nav>
+    </div>
+
+  </header>
+
+</body>
---
+++ vistas/productos.php
@@ -0,0 +1,119 @@
+<?php include 'head.php';?>
+
+
+	<section class="sectionproductos">
+		<h1 class="nosotros">
+		Productos
+		</h1>
+
+		<h3 class="lema">
+			En <b>AMERIXPORTA</b> contamos con produtos especializados y de buena calidad.
+		</h3>
+
+		<article class="contenproducc">
+
+		<article class="productounoo">
+			<article class="block">BLOCK</article>
+			<article class="imgblock"></article>
+			<article class="medidas">Medidas</article>
+			<article class="especificaciones">
+			*BLOCK DE 10X20X40 <br>
+			*BLOCK DE 12X20X40 <br>
+			*BLOCK DE 15X20X40 (3 HUECOS) <br>
+			*BLOCK DE 15X20X40 (2 HUECOS) <br>
+			*BLOCK DE 20X20X40
+			</article>
+		</article>
+
+		<article class="productodoss">
+			<article class="block">BOVEDILLA</article>
+			<article class="imgbobedilla"></article>
+			<article class="medidas">Medidas</article>
+			<article class="especificaciones">
+			*BOVEDILLA DE 15X20X56 <br>
+			*BOVEDILLA DE 15X25X56 <br>
+			*BOVEDILLA DE 20X25X56 <br>
+			*BOVEDILLA DE 24X25X56 <br>
+			*BOVEDILLA DE 30X25X56
+			</article>
+		</article>
+
+		<article class="productotress">
+			<article class="block">VIGAS PRETENSADAS</article>
+			<article class="imgvigas"></article>
+			<article class="medidas">Medidas</article>
+			<article class="especificaciones">
+			*VIGA 12-3 <br>
+			*VIGA 12-4 <br>
+			*VIGA 12-5 <br>
+			*VIGA 20-5 <br>
+			*VIGA 20-6
+			</article>
+		</article>
+
+	</article>
+
+	<article class="contenproducc">
+
+		<article class="productocuatro">
+			<article class="block">AGREGADOS</article>
+			<article class="imgagregado"></article>
+			<article class="medidas">Principales agregados</article>
+			<article class="especificaciones">
+			*GRAVA<br>
+			*POLVO<br>
+			<!-- *CONCRETOS<br> -->
+			*CAL HIDRATADA<br>
+			*ASFALTO
+			</article>
+		</article>
+
+		<article class="productocinco">
+			<article class="block">CONCRETO PREMEZCLADO</article>
+			<article class="imgconcreto"></article>
+			<article class="medidas">Tipos</article>
+			<article class="especificaciones">
+			*Premezclado Estándar <br>
+			*Arquitectónico y Decorativo <br>
+			*De Fraguado Rápido <br>
+			*Reforzado con Fibras <br>
+			*Concreto Poroso
+			</article>
+		</article>
+
+		<article class="productoseis">
+			<article class="block">PRODUCTOS DE ACERO</article>
+			<article class="imgaceros"></article>
+			<article class="medidas">Principales productos</article>
+			<article class="especificaciones">
+			*VARILLAS <br>
+			*ALAMBRE GALVANIZADO <br>
+			*ALAMBRON <br>
+			*ARMEX <br>
+			*CLAVOS
+			</article>
+		</article>
+
+	</article>
+
+<article class="boton">
+		 <a href="contacto.php" class="button" style="vertical-align:middle" ><span>Contáctanos</span></a>
+	</article>
+
+
+	</section>
+
+
+<span class="ir-arriba icon-arrow-bold-up"></span>
+
+  <?php include 'Pie.php';?>
+
+    <script type="text/javascript" src="../js/jquery.js"></script>
+  <script src="../js/main.js"></script>
+	<script src="../js/menuresponsivo.js"></script>
+  <script type="text/javascript" src="../js/iconoarriba.js"></script>
+  <script src="../js/active.js"></script>
+
+
+  </body>
+</html>
