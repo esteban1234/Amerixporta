99a91038845d0ad535bd5150ce8236572246e4cc
champiiy <albert1047@hotmail.com>
2016-12-20 18:30:02 -0600

    Agregando el meta

--- vistas/head.php
+++ vistas/head.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <link rel="icon" type="image/png" href="../img/favicon.png" />
     <title>AMERIXPORTA | SERVICIOS DE CONSTRUCCIÓN</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <META NAME="Title" content="Somos una compañía ubicada en la ciudad de Mérida, Yucatán, México, dedicados a ofrecer productos para la construcción, construcción de casas, remodelación, contrucción de carreteras, edificios y ampliaciones.">
     <META NAME="Author" CONTENT="[]"/>
     <META name="Description" content="Somos una compañía ubicada en la ciudad de Mérida, Yucatán, México, dedicados a ofrecer productos para la construcción, construcción de casas, remodelación, contrucción de carreteras, edificios y ampliaciones."/>
